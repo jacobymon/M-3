05767b22f20d05340520d633eeda40c764323ece
Jacoby Lockman <jlockman@g.hmc.edu>
2024-04-14 22:06:36 -0700

    "code passes the init tests"

--- UniversalQueue/test_Song.py
+++ UniversalQueue/test_Song.py
@@ -1,25 +1,34 @@
 import unittest
 from Song import Song
-import SongTest
+import json
 
 class TestSong(unittest.TestCase):
 
     #when integration comes we will add tests with patch and mocked requests
     #for now we will use a static json
+    def setUp(self):
+        with open('SongTest.json', 'r') as file:
+            self.SongTest_data = file.read()
+
+        with open('SongTest2.json', 'r') as file:
+            self.SongTest2_data = file.read()
+
+    
     def test_init(self):
-        song1 = Song(SongTest.json)
+        song1 = Song(self.SongTest_data)
 
         self.assertEqual(song1.uri, "asadfas")
-        self.assertEqual(song1.song_len, 123)
+        self.assertEqual(song1.s_len, 123)
         self.assertEqual(song1.name, "breh")
         self.assertEqual(song1.album, "the breh album")
         self.assertEqual(song1.artist, "dude")
         self.assertEqual(song1.id, None)
 
         #bad request in json
-        self.assertRaises(ValueError, Song.__init__, SongTest2.json)
+        self.assertRaises(ValueError, Song, self.SongTest2_data)
 
-
+if __name__ == "__main__":
+    unittest.main()
 
 
 
