04e56ac4f11eadeaaffc60e273c175d872f6aa31
Chichri <farnezelameyer@gmail.com>
2024-05-01 00:35:56 -0700

    Added routes and logic for visual queue

--- UniversalQueue/UniversalQueueDesign.py
+++ UniversalQueue/UniversalQueueDesign.py
@@ -307,7 +307,10 @@ def return_results_from_url():
 def submit_song():
 
     song_data = request.get_json()
+    print("SONGDATA SONGDATA BEFORE", song_data)
+    song_data['search_results']['name'] = song_data['search_results']['title']
     song_data = json.dumps(song_data)
+    print("SONGDATA SONGDATA", song_data)
     song = Song(song_data)
 
     UQ.insert(song)
@@ -315,6 +318,29 @@ def submit_song():
 
     print(UQ.data) 
     return song_data
+
+@app.route('/request_update', methods=['GET', 'POST'])
+@cross_origin()
+def update_visual_queue():
+
+    # current_queue_data = UQ.datak
+    # current_queue_data = json.dumps(current_queue_data)
+    data = []
+    for i in range(len(UQ.data)):
+        songObject = {
+                'name': UQ.data[i].name,
+                'artist': UQ.data[i].artist,
+                'albumcover': "",
+                'submissionID': 1,
+                'id': ""
+                    }
+        # print( "NAMENAMENAMENAMENAMENANEMEANE " + UQ.data[i].name, songObject['name'])
+        data.append(songObject)
+
+    jsonData = json.dumps(data)
+    # print('#################' + jsonData + '#################')
+    return jsonData
+
     
 if __name__ == '__main__':
     with open('../m3-frontend/.env', 'w') as f_obj:
